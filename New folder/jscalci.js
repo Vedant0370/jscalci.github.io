48fbdc3c8025ac15f8c478e3de8caeaaa4c62ee3
Vedant0370 <114738289+Vedant0370@users.noreply.github.com>
2022-12-20 18:16:07 +0530

    Add files via upload

---
+++ New folder/jscalci.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+function add(num1 , num2){
+    
+    var num1 = document.forms['form1']['num1'].value;
+    
+    var num2 = document.forms['form1']['num2'].value;
+
+    alert(num1 + num2);
+}
+
+
+function sub(num1 , num2){
+
+    alert(num1 - num2)
+}
